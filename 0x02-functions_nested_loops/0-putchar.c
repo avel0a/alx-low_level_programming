701bbbb226775866afc77043c790c667fd5d4901
avel0a <abelaman618@gmail.com>
2022-04-12 21:26:41 +0300

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char word[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
