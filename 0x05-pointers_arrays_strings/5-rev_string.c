f5179cc0873fe97d1bf40abc08ca7e0c47f00508
avel0a <abelaman618@gmail.com>
2022-04-18 20:20:54 +0300

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @s : string of _ to be printed
+ * Return:void
+ */
+
+void rev_string(char *s)
+{
+int l, i;
+char *begin_ptr, *end_ptr, ch;
+l = strlen(s);
+begin_ptr = s;
+end_ptr = s;
+for (i = 0; i < l - 1; i++)
+{
+end_ptr++;
+}
+for (i = 0; i < l / 2; i++)
+{
+ch = *end_ptr;
+*end_ptr = *begin_ptr;
+*begin_ptr = ch;
+begin_ptr++;
+end_ptr--;
+}
+}
