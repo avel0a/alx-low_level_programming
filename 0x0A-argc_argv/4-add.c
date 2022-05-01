7e8fe03a6211408ebb55e91a3714911d6db5416b
avel0a <abelaman618@gmail.com>
2022-05-01 19:46:11 +0300

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - adds all intigers in an argument
+ * @argc:int
+ * @argv:array of strings
+ * Return: sum if all numeric(intiger) values
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int j;
+int num;
+int sum;
+char *s;
+sum = 0;
+num = 0;
+i = 1;
+while (i < argc)
+{
+s = argv[i];
+j = 0;
+while (s[j] != '\0')
+{
+if (s[j] < '0' || s[j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+j++;
+}
+i++;
+}
+for (i = 1; i < argc; i++)
+{
+num = atoi(argv[i]);
+sum = sum + num;
+}
+printf("%d\n", sum);
+return (0);
+}
