b1f0662a0a95b176a5c60fd139b66f0522cbfa7e
avel0a <abelaman618@gmail.com>
2022-05-01 19:46:07 +0300

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: int
+ * @argv: array of strings
+ * Return: sum
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+
+	if (argc < 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
+}
