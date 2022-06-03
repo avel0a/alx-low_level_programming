6e6f305418dabfcd4fe2042f1de39f4061f1cf3c
avel0a <abelaman618@gmail.com>
2022-06-03 22:37:17 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,34 @@
-/*
- * File: 1-create_file.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
+#include "main.h"
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * create_file - creates a file and fills it with text content
+ * @filename: name of file created
+ * @text_content: text of file created
+ * Return: 1 if success
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd;
+	int writen;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+		if (fd == -1)
+			return (-1);
+
+		if (text_content == NULL)
+			return (1);
+
+		while (text_content[len] != '\0')
 			len++;
-	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+		writen = write(fd, text_content, len);
+		if (writen == -1)
+			return (-1);
 
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
+	close(fd);
 
 	return (1);
 }
